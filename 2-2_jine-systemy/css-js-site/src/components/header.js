b75900f6d07172e4ed88a157d3d86d2cb8217837
Lukáš Svoboda <svobodal@cngroup.dk>
2019-11-24 14:03:31 +0100

    Bem components, js frameworks

---
+++ 2-2_jine-systemy/css-js-site/src/components/header.js
@@ -0,0 +1,48 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import styled from "@emotion/styled"
+
+const HeaderTitle = styled.h1`
+  margin: 20px 0;
+  text-align: center;
+`;
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <HeaderTitle>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </HeaderTitle>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
