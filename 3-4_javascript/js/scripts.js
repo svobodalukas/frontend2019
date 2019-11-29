be9f39b737172711c01d757071ed4c0cdce66a9b
Lukáš Svoboda <svobodal@cngroup.dk>
2019-11-29 16:18:22 +0100

    Boostrap

--- 3-4_javascript/js/scripts.js
+++ 3-4_javascript/js/scripts.js
@@ -1,15 +1,33 @@
 // JS - query selectors
 
 // změna CSS třídy: box - box.active toggle
+const btnToggleClass = document.querySelector('#btnToggleClass');
+const boxes = document.querySelectorAll('.box');
 
+btnToggleClass.onclick = function() {
+  boxes.forEach(box => {
+    box.classList.toggle('box-active');
+  });
+}
 
 // změna CSS proměnné - getComputedStyle().getPropertyValue /  style.setProperty
-
+const elCssVar = document.querySelector('#greenTitle');
+const currentVar = getComputedStyle(elCssVar).getPropertyValue("--color-title");
+console.log(`současná barva: ${currentVar}`);
+elCssVar.style.setProperty("--color-title", "green");
 
 // změna proměnné na root elementu - document.documentElement
 
 
 // zatržení všech checkboxů .checked
+const btnCheckAll = document.querySelector('#btnCheckAll');
+const checks = document.querySelectorAll('.table-check');
+
+btnCheckAll.onclick = function() {
+  checks.forEach(check => {
+    check.checked = !check.checked;
+  });
+}
 
 
 // async / await
@@ -20,7 +38,7 @@ async function getUserAsync(name)
   return data;
 }
 
-// getUserAsync('svobodalukas')
-//   .then(data => console.log(data));
+ getUserAsync('svobodalukas')
+   .then(data => console.table(data));
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
